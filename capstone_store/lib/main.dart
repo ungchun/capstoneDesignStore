da67ec73ec53f1776a92a1b0d7a2e93582d4b30f
op-9-79-1 <68549862+op-9-79-1@users.noreply.github.com>
2020-10-20 22:34:40 +0900

    Update main.dart

--- capstone_store/lib/main.dart
+++ capstone_store/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:capstone_store/tap_state.dart';
@@ -13,6 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+       theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Colors.brown[200],
+        accentColor: Colors.pink,
+      ),
       home: Tap_Page(),
     );
   }
