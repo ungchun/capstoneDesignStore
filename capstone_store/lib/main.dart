a306e95a6564286cd2352989492e0eaf85c45b2e
op-9-79-1 <68549862+op-9-79-1@users.noreply.github.com>
2020-10-20 22:43:01 +0900

    Update main.dart

--- capstone_store/lib/main.dart
+++ capstone_store/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-       theme: ThemeData(
+      theme: ThemeData(
         primarySwatch: Colors.blue,
         primaryColor: Colors.brown[200],
         accentColor: Colors.pink,
