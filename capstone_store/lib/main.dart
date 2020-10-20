16ed457cd22f04a4bfd08224c14c020420bcf2ec
ungchun <leedool3003@naver.com>
2020-10-20 18:26:35 +0900

    1

--- capstone_store/lib/main.dart
+++ capstone_store/lib/main.dart
@@ -9,76 +9,71 @@ Future<void> main() async {
   runApp(MyApp());
 }
 
-
-// void main() {
-//   runApp(MyApp());
-// }
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
+      home: Tap_Page(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+// class MyHomePage extends StatefulWidget {
+//   MyHomePage({Key key, this.title}) : super(key: key);
 
-  final String title;
+//   final String title;
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
 
-class _MyHomePageState extends State<MyHomePage> {
+// class _MyHomePageState extends State<MyHomePage> {
 
-  // firestore 데이터 읽어오기 test
-  FirebaseFirestore firestore = FirebaseFirestore.instance;
-  void test() {
-    String title = "";
-    firestore.collection("test").doc("1").get().then((DocumentSnapshot ds) {
-      title = ds.data()['name'];
-      print("asd " + title);
-    });
-  }
+//   // firestore 데이터 읽어오기 test
+//   FirebaseFirestore firestore = FirebaseFirestore.instance;
+//   void test() {
+//     String title = "";
+//     firestore.collection("test").doc("1").get().then((DocumentSnapshot ds) {
+//       title = ds.data()['name'];
+//       print("asd " + title);
+//     });
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("주문목록"),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          FloatingActionButton(
-            onPressed: () {
-              test();
-            },
-          ),
-          Text("ㅁㅁ"),
-          Text("ㅁㅁ"),
-          Text("ㅁㅁ"),
-          Text("ㅁㅁ"),
-          Text("ㅁㅁ"),
-          Text("ㅁㅁ"),
-          Text("ㅁㅁ"),
-          Row(
-            children: [
-              Container(
-                child: Text(
-                  "안녕",
-                  style: TextStyle(color: Colors.red, fontSize: 50),
-                ),
-              ),
-              Container(child: Text("김성훈입니다")),
-            ],
-          ),
-          Text("ㅁㅁ"),
-        ],
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("주문목록"),
+//       ),
+//       body: Column(
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//         children: [
+//           FloatingActionButton(
+//             onPressed: () {
+//               test();
+//             },
+//           ),
+//           Text("ㅁㅁ"),
+//           Text("ㅁㅁ"),
+//           Text("ㅁㅁ"),
+//           Text("ㅁㅁ"),
+//           Text("ㅁㅁ"),
+//           Text("ㅁㅁ"),
+//           Text("ㅁㅁ"),
+//           Row(
+//             children: [
+//               Container(
+//                 child: Text(
+//                   "안녕",
+//                   style: TextStyle(color: Colors.red, fontSize: 50),
+//                 ),
+//               ),
+//               Container(child: Text("김성훈입니다")),
+//             ],
+//           ),
+//           Text("ㅁㅁ"),
+//         ],
+//       ),
+//     );
+//   }
+// }
