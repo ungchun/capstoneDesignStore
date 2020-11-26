56931feb87ea4a215c2a988aefda7649cc2f4c34
op-9-79-1 <68549862+op-9-79-1@users.noreply.github.com>
2020-11-26 13:54:05 +0900

    20_11_26_02

--- capstone_store/lib/store/store_page.dart
+++ capstone_store/lib/store/store_page.dart
@@ -1,3 +1,4 @@
+import 'package:capstone_store/size_config.dart';
 import 'package:capstone_store/store/order_counter.dart';
 import 'package:flutter/material.dart';
 
@@ -9,15 +10,38 @@ class StorePage extends StatefulWidget {
 class _StorePageState extends State<StorePage> {
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text(
           '가게 상황',
-          style: TextStyle(fontSize: 25),
+          style: TextStyle(fontSize: getProportionateScreenWidth(20)),
         ),
       ),
-      body: OrderCounter(),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: getProportionateScreenWidth(20)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'hi',
+                      style: TextStyle(
+                          fontSize: getProportionateScreenWidth(18),
+                          color: Colors.black),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
